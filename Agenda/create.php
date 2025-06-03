156b6159f4824e4c3850699e709b0d36b64710ce
brunomecca-dev <bruno.mecca@outlook.com>
2025-06-03 10:21:37 -0300

    commit 07

--- Agenda/create.php
+++ Agenda/create.php
@@ -1,7 +1,26 @@
     <?php
     include_once("templates/header.php");
     ?>
-    <h1>Criando Contato</h1>
+    <div class="container">
+        <?php include_once("templates/backbtn.php"); ?>
+        <h1 id="main-title">Criando Contato</h1>
+        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+            <input type="hidden" name="type" value="create">
+            <div class="form-group">
+                <label for="name">Nome do Contato:</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome do contato" required>
+            </div>
+            <div class="form-group">
+                <label for="phone">Telefone do Contato:</label>
+                <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" required>
+            </div>
+            <div class="form-group">
+                <label for="observations">Observações:</label>
+                <input type="text" class="form-control" id="observations" name="observations" placeholder="Insira as observações" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Criar Contato</button>
+        </form>
+    </div>
     <?php
     include_once("templates/footer.php");
     ?>
