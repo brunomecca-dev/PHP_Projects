0fae1b068ce270294a6526b21cdd105454578175
bruno.meccaA <brumec2013@hotmail.com>
2025-06-04 19:59:16 -0300

    commit 08

--- Agenda/edit.php
+++ Agenda/edit.php
@@ -7,15 +7,15 @@ include_once("templates/header.php");
     <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
         <input type="hidden" name="type" value="edit">
         <input type="hidden" name="id" value="<?= $contact['id'] ?>">
-        <div class="form-group">
+        <div class="mb-3">
             <label for="name">Nome do contato:</label>
             <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" value="<?= $contact['name'] ?>" required>
         </div>
-        <div class="form-group">
+        <div class="mb-3">
             <label for="phone">Telefone do contato:</label>
             <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" value="<?= $contact['phone'] ?>" required>
         </div>
-        <div class="form-group">
+        <div class="mb-3">
             <label for="observations">Observações:</label>
             <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Insira as observações" rows="3"><?= $contact['observations'] ?></textarea>
         </div>
