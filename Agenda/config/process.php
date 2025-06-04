8b68a196c9bf63a5f9b3f1059a3125d82dfd02ab
bruno.meccaA <brumec2013@hotmail.com>
2025-06-03 21:00:27 -0300

    commit 07

--- Agenda/config/process.php
+++ Agenda/config/process.php
@@ -4,23 +4,61 @@ session_start();
 include_once("connection.php");
 include_once("url.php");
 
-$id = null;;
+$data = $_POST;
 
-if (!empty($_GET["id"])) {
-    $id = $_GET["id"];
-}
+// MODIFICAÇÔES NO BANCO
+if (!empty($data)) {
 
-// Retorna dado de um contato
-if (!empty($id)) {
-    $query = "SELECT * FROM contacts WHERE id = :id";
+    // CRIAR CONTATO
+    if ($data["type"] === "create") {
+        $name = $data["name"];
+        $phone = $data["phone"];
+        $observations = $data["observations"];
+    }
+
+
+
+    $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
+
     $stmt = $conn->prepare($query);
-    $stmt->bindParam(":id", $id);
-    $stmt->execute();
-    $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt->bindParam(":name", $name);
+    $stmt->bindParam(":phone", $phone);
+    $stmt->bindParam(":observations", $observations);
+
+    try {
+        $stmt->execute();
+        $_SESSION["msg"] = "CONTATO CRIADO COM SUCESSO!";
+    } catch (PDOException $e) {
+        // erro na conexão
+        $error = $e->getMessage();
+        echo "Erro: $error";
+    }
+    // REDIRECT HOME
+    header("Location:" . $BASE_URL . "../index.php");
+
+    // SELEÇÂO DE DADOS    
 } else {
-    $contacts = [];
-    $query = "SELECT * FROM contacts";
-    $stmt = $conn->prepare($query);
-    $stmt->execute();
-    $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $id = null;;
+
+    if (!empty($_GET["id"])) {
+        $id = $_GET["id"];
+    }
+
+    // Retorna dado de um contato
+    if (!empty($id)) {
+        $query = "SELECT * FROM contacts WHERE id = :id";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+    } else {
+        $contacts = [];
+        $query = "SELECT * FROM contacts";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
+
+// FECHAR CONEXÃO
+$conn = null;
--- Agenda/create.php
+++ Agenda/create.php
@@ -6,17 +6,17 @@
         <h1 id="main-title">Criando Contato</h1>
         <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
             <input type="hidden" name="type" value="create">
-            <div class="form-group">
+            <div class="mb-3">
                 <label for="name">Nome do Contato:</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome do contato" required>
             </div>
-            <div class="form-group">
+            <div class="mb-3">
                 <label for="phone">Telefone do Contato:</label>
                 <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" required>
             </div>
-            <div class="form-group">
+            <div class="mb-3">
                 <label for="observations">Observações:</label>
-                <input type="text" class="form-control" id="observations" name="observations" placeholder="Insira as observações" required>
+                <input type="text" class="form-control" id="observations" name="observations" placeholder="Insira as observações" rows="3" required>
             </div>
             <button type="submit" class="btn btn-primary">Criar Contato</button>
         </form>
