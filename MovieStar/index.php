7470396a6876bae72cd777a467e873709a2fd8f1
bruno.meccaA <brumec2013@hotmail.com>
2025-06-17 20:25:15 -0300

    commit 01

---
+++ MovieStar/index.php
@@ -0,0 +1,30 @@
+<?php
+require_once("globals.php");
+require_once("db.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MovieStar</title>
+    <link rel="short icon" href="img/moviestar.ico" />
+    <!--- Bootstrap CSS --->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.6/css/bootstrap.css" integrity="sha512-d5LqZ/jQTKvIEJnGakV9MhG50OFtvfqKPW4/3TkXm0lBlivNYK8mrfRqDsRu0VysLcgB1Y4CaOb2fCkp/wGPXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--- Font Awesome --->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--- CSS do Projeto --->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <h1>Welcome to MovieStar</h1>
+    <i class="far fa-plus-square"></i>
+    <!--- Bootstrap JS --->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.6/js/bootstrap.js" integrity="sha512-prYbThJxUeY3wbEsIacbPmvnxgurr1UT59iUAkkEqTZ5klHQxAEPBUujqFK9X6X2RqunR9GFWiNg7bhEQKRYTQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+
+</html>
