497acc744a289537f97718df17e50bc7d5765db0
brunomecca-dev <bruno.mecca@outlook.com>
2025-07-10 20:50:46 -0300

    commit 04

--- MovieStar/db.php
+++ MovieStar/db.php
@@ -2,11 +2,11 @@
 
 $db_name = "moviestar";
 $db_host = "localhost";
-$db_user = "admin";
-$db_pass = "admin";
+$db_user = "root";
+$db_pass = "";
 
-$conn = new PDO("mysql:dbname=" . $dab_name . ";host=" . $db_host, $db_user, $db_pass);
+$conn = new PDO("mysql:dbname=" . $db_name . ";host=" . $db_host, $db_user, $db_pass);
 
-//Habilitar erros PDO
+// Habilitar erros PDO
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
