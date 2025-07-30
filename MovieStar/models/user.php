ecedeee24fe228ea806561420aa172cdcc47e786
Bruno Mecca <bruno.mecca@outlook.com>
2025-07-30 09:33:40 -0300

    commit 08

--- MovieStar/models/user.php
+++ MovieStar/models/user.php
@@ -12,6 +12,11 @@ class User
     public $bio;
     public $token;
 
+    public function getFullname($user)
+    {
+        return $user->name . " " . $user->lastname;
+    }
+
     public function generateToken()
     {
         return bin2hex(random_bytes(50));
@@ -20,6 +25,11 @@ class User
     public function generatePassword($password)
     {
         return password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    public function imageGenerateName()
+    {
+        return bin2hex(random_bytes(60) . ".jpg");
     }
 }
 
--- MovieStar/user_process.php
+++ MovieStar/user_process.php
@@ -34,10 +34,54 @@ if ($type === "update") {
     $userData->email = $email;
     $userData->bio = $bio;
 
+    // Upload da imagem de perfil
+    if (isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
+        $image = $_FILES["image"];
+        $imageTypes = ["image/jpeg", "image/jpg", "image/png"];
+        $jpgArray = ["image/jpeg", "image/jpg"];
+
+        //Checar o tipo de imagem
+        if (in_array($image["type"], $imageTypes)) {
+            // checar se é jpg
+            if (in_array($image["type"], $jpgArray)) {
+                $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+                // Imagem é png
+            } else {
+                $imageFile = imagecreatefrompng($image["tmp_name"]);
+            }
+            $imageName = $user->imageGenerateName($image["type"]);
+
+            imagejpeg($imageFile, "./img/users/" . $imageName, 100);
+
+            $userData->image = $imageName;
+        } else {
+            $message->setMessage("Tipo inválido de imagem, insira png ou jpg!", "error", "back");
+        }
+    }
+
     $userDao->update($userData);
 
     // Atualizar senha do usuário
 } else if ($type === "changepassword") {
+    // Receber dados do post
+    $password = filter_input(INPUT_POST, "password");
+    $confirmpassword = filter_input(INPUT_POST, "confirmpassword");
+
+    //Resgata dados do usuário
+    $userData = $userDao->verifyToken();
+    $id = $userData->id;
+
+    if ($password == $confirmpassword) {
+        $user = new User();
+
+        $finalPassword = $user->generatePassword($password);
+
+        $user->id = $id;
+
+        $userDao->changePassword($user);
+    } else {
+        $message->setMessage("As senhas não são iguais!", "error", "back");
+    }
 } else {
     $message->setMessage("Informações inválidas!", "error", "index.php");
 }
